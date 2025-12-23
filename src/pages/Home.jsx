7e5a8b0bbf20947ddfbfd8e7835a6cb1b1a9c9d2
mostafameesr1 <mostafameesr4@gmail.com>
2025-12-23 18:16:47 +0200

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,10 @@
+import ROMCard from "../components/ROMCard";
+import roms from "../data/roms.json";
 
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {roms.map((rom, idx) => <ROMCard key={idx} rom={rom} />)}
+    </div>
+  );
+}
