5e6be62d40904efc2050e1fe81537135cac0744c
mostafameesr1 <mostafameesr4@gmail.com>
2025-12-23 20:29:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import ROMPage from "./pages/ROMPage";
 
-export default function App() {
+function App() {
   return (
-    <>
-      <Navbar />
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/rom/:id" element={<ROMPage />} />
+        {/* يمكنك إضافة صفحات ROM هنا */}
       </Routes>
-      <Footer />
-    </>
+    </Router>
   );
 }
+
+export default App;
