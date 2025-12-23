24ad9a243ee487aca551eeff384ef49df2bfe107
mostafameesr1 <mostafameesr4@gmail.com>
2025-12-23 18:42:05 +0200

    Pass index prop to ROMCard in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,9 @@ import roms from "../data/roms.json";
 export default function Home() {
   return (
     <div className="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {roms.map((rom, idx) => <ROMCard key={idx} rom={rom} />)}
+      {roms.map((rom, index) => (
+        <ROMCard key={index} rom={rom} index={index} />
+      ))}
     </div>
   );
 }
