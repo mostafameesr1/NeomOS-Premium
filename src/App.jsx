337bb0d071862f7ffe5c104b2fa3171d7cc3a860
mostafameesr1 <mostafameesr4@gmail.com>
2025-12-23 20:34:41 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ROMPage from "./pages/ROMPage"; // صفحة كل ROM
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* يمكنك إضافة صفحات ROM هنا */}
+        <Route path="/rom/:id" element={<ROMPage />} />
       </Routes>
     </Router>
   );
